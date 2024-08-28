49d0bea38e8727aeb782d520d505007f032a9472
taewoo <rlaxodn322@gmail.com>
2024-08-28 16:43:40 +0900

    보안 헤더 설정

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,15 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 보안 헤더 설정
+  app.use((req, res, next) => {
+    res.setHeader('X-DNS-Prefetch-Control', 'off');
+    res.setHeader('X-Frame-Options', 'SAMEORIGIN');
+    res.setHeader('X-Content-Type-Options', 'nosniff');
+    res.setHeader('Referrer-Policy', 'no-referrer');
+    next();
+  });
   app.enableCors({
     origin: 'http://localhost:3000',
     credentials: true,
