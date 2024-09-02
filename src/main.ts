a744696de6847b6c4f4fefd53025cebe77b185b4
taewoo <rlaxodn322@gmail.com>
2024-09-02 16:40:40 +0900

    helmet 추가 / rateLimit 추가하여 ip 주소에대해 요청 빈도 제한

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
+
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // 보안 헤더 설정
+  // Rate Limiting 설정
+  const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15분
+    max: 100, // 각 IP당 최대 100 요청
+  });
+  app.use(limiter);
+
+  // Helmet을 사용한 보안 헤더 설정
+  app.use(helmet());
+
+
+
+  // 보안 헤더 추가 설정
   app.use((req, res, next) => {
     res.setHeader('X-DNS-Prefetch-Control', 'off');
     res.setHeader('X-Frame-Options', 'SAMEORIGIN');
@@ -14,10 +29,12 @@ async function bootstrap() {
     res.setHeader('Referrer-Policy', 'no-referrer');
     next();
   });
+
   app.enableCors({
     origin: 'http://localhost:3000',
     credentials: true,
   });
+
   await app.listen(3001);
 }
 bootstrap();
