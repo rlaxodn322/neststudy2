244312e88f6ed8b8212bd936b4b2e22d53d8eb0a
taewoo <rlaxodn322@gmail.com>
2024-09-04 16:29:26 +0900

    보안 헤더 typescript 추가

--- src/main.ts
+++ src/main.ts
@@ -19,16 +19,20 @@ async function bootstrap() {
   // Helmet을 사용한 보안 헤더 설정
   app.use(helmet());
 
-
-
   // 보안 헤더 추가 설정
-  app.use((req, res, next) => {
-    res.setHeader('X-DNS-Prefetch-Control', 'off');
-    res.setHeader('X-Frame-Options', 'SAMEORIGIN');
-    res.setHeader('X-Content-Type-Options', 'nosniff');
-    res.setHeader('Referrer-Policy', 'no-referrer');
-    next();
-  });
+  app.use(
+    (
+      req: any,
+      res: { setHeader: (arg0: string, arg1: string) => void },
+      next: () => void,
+    ) => {
+      res.setHeader('X-DNS-Prefetch-Control', 'off');
+      res.setHeader('X-Frame-Options', 'SAMEORIGIN');
+      res.setHeader('X-Content-Type-Options', 'nosniff');
+      res.setHeader('Referrer-Policy', 'no-referrer');
+      next();
+    },
+  );
 
   app.enableCors({
     origin: 'http://localhost:3000',
